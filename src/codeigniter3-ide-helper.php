57492970eaf74daf12618000c99129b561778269
leohu <cqleohu@qq.com>
2016-06-18 21:54:44 +0800

    add ide helper

---
+++ src/codeigniter3-ide-helper.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @package	CodeIgniter
+ * @author	leohu <alpha1130@gmail.com>
+ * @since	2016-06-18 21:49
+ * @version 1.0.0
+ */
+
+/**
+ *
+ * @property CI_Benchmark               $benchmark
+ * @property CI_Cache                   $cache
+ * @property CI_Calendar                $calendar
+ * @property CI_Cart                    $cart
+ * @property CI_Config                  $config
+ * @property CI_Email                   $email
+ * @property CI_Encrypt                 $encrypt
+ * @property CI_Exceptions              $exceptions
+ * @property CI_Upload                  $upload
+ * @property CI_Form_validation         $form_validation
+ * @property CI_Ftp                     $ftp
+ * @property CI_Image_lib               $image_lib
+ * @property CI_Input                   $input
+ * @property CI_Javascript              $javascript
+ * @property CI_Lang                    $lang
+ * @property CI_Loader                  $load
+ * @property CI_Migration               $migration
+ * @property CI_Output                  $output
+ * @property CI_Pagination              $pagination
+ * @property CI_Parser                  $parser
+ * @property CI_Security                $security
+ * @property CI_Session                 $session
+ * @property CI_Table                   $table
+ * @property CI_Trackback               $trackback
+ * @property CI_Typography              $typography
+ * @property CI_Unit_test               $unit_test
+ * @property CI_URI                     $uri
+ * @property CI_User_agent              $user_agent
+ * @property CI_Xmlrpc                  $xmlrpc
+ * @property CI_Xmlrpcs                 $xmlrpcs
+ * @property CI_Zip                     $zip
+ * @property CI_DB_query_builder        $db
+ */
+class bde1c5b421 {}
+class CI_Controller extends bde1c5b421 {public function __construct(){}}
+class CI_Model extends bde1c5b421 {public function __construct(){}}
